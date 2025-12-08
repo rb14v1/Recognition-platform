7f04013b170c291dfdaeda9ecdfbd0760b1d2f8c
nidish.nidish <nidish.nidish@version1.com>
2025-12-08 12:48:21 +0530

    Committee page integrated codes

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ import {
   TrendingUp,
   Gavel
 } from "@mui/icons-material";
-import { Tooltip, IconButton, Typography } from "@mui/material";
+import { Tooltip, IconButton} from "@mui/material";
 
 interface SidebarProps {
   activeSection: string;
